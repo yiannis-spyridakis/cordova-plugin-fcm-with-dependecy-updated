88e56c0ece678a577ca7d4ad6cf5f4b00f1b6d12
yiannis-spyridakis <yiannis.spyridakis@logismix.com>
2024-03-11 11:52:02 +0200

    Update FCMPlugin.java
    Commented out unused import that was causing build errors in FCMPlugin.java

--- src/android/com/gae/scaffolder/plugin/FCMPlugin.java
+++ src/android/com/gae/scaffolder/plugin/FCMPlugin.java
@@ -11,7 +11,7 @@ import com.gae.scaffolder.plugin.interfaces.*;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.iid.InstanceIdResult;
+// import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.apache.cordova.CallbackContext;
